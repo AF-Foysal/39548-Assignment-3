cb28e7fefdea8d7f24689374d2fb922e416909b6
JoshuaRamc <joshuaramcharan818@gmail.com>
2023-09-29 23:59:15 -0400

    comment dropdown added

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ let generateRowButton = document.body.querySelector("#generate-row");
 var rowCounter = 0;
 var columnCounter = 0;
 
-function columnCreator(){
-    
-}
+function columnCreator() {}
 generateColumnButton.addEventListener("click", () => {
   if (rowCounter === 0) {
+    rowCounter += 1;
     columnCounter += 1;
     let myColumn = document.createElement("td");
     myColumn.id = "column" + columnCounter;
@@ -29,30 +28,31 @@ generateColumnButton.addEventListener("click", () => {
       currentRow.appendChild(myColumn);
     }
   }
-  console.log(columnCounter);
+  console.log("ColumnCount: ", columnCounter);
 });
 
 generateRowButton.addEventListener("click", () => {
-  
-    rowCounter += 1;
-    let myRow = document.createElement("tr");
-    let rowData = document.createElement("td");
-    myRow.id = "row" + rowCounter;
-    rowData.className = "box";
-    grid.appendChild(myRow);
-    myRow.appendChild(rowData);
-    myRow.appendChild(rowData);
-   
-//   else {
-//     rowCounter += 1;
-//     for (let i = 1; i < columnCounter; i++) {
-//       let myRow = document.createElement("tr");
-//       let rowData = document.createElement("td");
-//       let currentColumn = document.getElementById("column" + i);
-//       myRow.id = "row" + rowCounter;
-//       rowData.className = "box";
-//       grid.appendChild(myRow);
-//       grid.appendChild(rowData);
-//     }
-//   }
+  rowCounter += 1;
+  let myRow = document.createElement("tr");
+  let rowData = document.createElement("td");
+  myRow.id = "row" + rowCounter;
+  rowData.className = "box";
+  grid.appendChild(myRow);
+  myRow.appendChild(rowData);
+  myRow.appendChild(rowData);
+
+  //   else {
+  //     rowCounter += 1;
+  //     for (let i = 1; i < columnCounter; i++) {
+  //       let myRow = document.createElement("tr");
+  //       let rowData = document.createElement("td");
+  //       let currentColumn = document.getElementById("column" + i);
+  //       myRow.id = "row" + rowCounter;
+  //       rowData.className = "box";
+  //       grid.appendChild(myRow);
+  //       grid.appendChild(rowData);
+  //     }
+  //   }
+
+  console.log("RowCount: ", rowCounter);
 });
